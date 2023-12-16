0d8c11cd31d277b7999cb3b6d19e29d7420e412d
YeonJeong Kim <65206075+yjeong-k@users.noreply.github.com>
2023-12-17 00:42:09 +0900

    Modify typo

--- code/comparison/bard.py
+++ code/comparison/bard.py
@@ -1,6 +1,6 @@
 # Get answers from Bard baseline model for comparison
 # Input : a CSV file with a column containing answer-generating prompts
-# Output : a CSV file with a newly added column conatining each corresponding answer for the prompts
+# Output : a CSV file with a newly added column containing each corresponding answer for the prompts
 
 from bardapi import BardCookies
 import pandas as pd
