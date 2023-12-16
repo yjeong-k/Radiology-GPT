75a73996a06d9b27b2c5ef24e84296ed11a8682d
YeonJeong Kim <65206075+yjeong-k@users.noreply.github.com>
2023-12-17 00:42:42 +0900

    Modify typo

--- code/comparison/medAlpaca.py
+++ code/comparison/medAlpaca.py
@@ -1,6 +1,6 @@
 # Get answers from medAlpaca baseline model for comparison
 # Input : a CSV file with a column containing answer-generating prompts
-# Output : a CSV file with a newly added column conatining each corresponding answer for the prompts
+# Output : a CSV file with a newly added column containing each corresponding answer for the prompts
 
 from transformers import pipeline, AutoTokenizer
 import argparse
