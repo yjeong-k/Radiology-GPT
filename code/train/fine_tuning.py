6b6f24f557420e3aafb940da2ab965a095077930
sangin514 <sangin514@naver.com>
2023-12-17 19:35:24 +0900

    feat: implementing fine_tuning module

---
+++ code/train/fine_tuning.py
@@ -0,0 +1,222 @@
+# Adapted From https://github.com/tatsu-lab/stanford_alpaca/blob/main/train.py
+
+#    Copyright 2023 Rohan Taori, Ishaan Gulrajani, Tianyi Zhang, Yann Dubois, Xuechen Li
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+
+# python train/instruction_ft.py --model_name_or_path "meta-llama/Llama-2-7b-chat-hf" --data_path "/workspace/train/training_data.jsonl" --output_dir "/workspace/train/ft_checkpoint/" --num_train_epochs 2 --per_device_train_batch_size 4 --per_device_eval_batch_size 1 --gradient_accumulation_steps 8 --evaluation_strategy "no" --save_strategy "epoch" --learning_rate 2e-4 --weight_decay 0. --warmup_ratio 0.03 --lr_scheduler_type "cosine" --logging_steps 1 --model_max_length 4096 --gradient_checkpointing True --ddp_timeout 18000
+
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+import copy
+import io
+import json
+import logging
+from dataclasses import dataclass
+from typing import Dict, Sequence
+from peft import get_peft_model, LoraConfig, TaskType, prepare_model_for_kbit_training
+
+import torch
+import transformers
+from torch.utils.data import Dataset
+from trl import SFTTrainer
+
+from utils import *
+
+
+IGNORE_INDEX = -100
+
+PROMPT = """You are an intelligent clinical languge model.
+Below is a snippet of patient's radiology report and a following instruction from radiologist.
+Write a response that appropriately completes the instruction.
+The response should provide the accurate answer to the instruction, while being concise.
+
+[Radiology Report Begin]
+{report}
+[Radiology Report End]
+
+[Instruction Begin]
+{instruction}
+[Instruction End]
+"""
+
+
+def jload(f, mode="r"):
+    """Load a .json file into a dictionary."""
+    if not isinstance(f, io.IOBase):
+        f = open(f, mode=mode)
+    jdict = json.load(f)
+    f.close()
+    return jdict
+
+
+def _tokenize_fn(
+    strings: Sequence[str], tokenizer: transformers.PreTrainedTokenizer
+) -> Dict:
+    """Tokenize a list of strings."""
+    tokenized_list = [
+        tokenizer(
+            text,
+            return_tensors="pt",
+            padding="longest",
+            max_length=tokenizer.model_max_length,
+            truncation=True,
+        )
+        for text in strings
+    ]
+    input_ids = labels = [tokenized.input_ids[0] for tokenized in tokenized_list]
+    input_ids_lens = labels_lens = [
+        tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item()
+        for tokenized in tokenized_list
+    ]
+    return dict(
+        input_ids=input_ids,
+        labels=labels,
+        input_ids_lens=input_ids_lens,
+        labels_lens=labels_lens,
+    )
+
+
+def preprocess(
+    sources: Sequence[str],
+    targets: Sequence[str],
+    tokenizer: transformers.PreTrainedTokenizer,
+) -> Dict:
+    """Preprocess the data by tokenizing."""
+    examples = [s + t for s, t in zip(sources, targets)]
+    examples_tokenized, sources_tokenized = [
+        _tokenize_fn(strings, tokenizer) for strings in (examples, sources)
+    ]
+    input_ids = examples_tokenized["input_ids"]
+    labels = copy.deepcopy(input_ids)
+    for label, source_len in zip(labels, sources_tokenized["input_ids_lens"]):
+        label[:source_len] = IGNORE_INDEX
+    return dict(input_ids=input_ids, labels=labels)
+
+
+class SupervisedDataset(Dataset):
+    """Dataset for supervised fine-tuning."""
+
+    def __init__(self, data_path: str, tokenizer: transformers.PreTrainedTokenizer):
+        super(SupervisedDataset, self).__init__()
+        logging.warning("Loading data...")
+        list_data_dict = jload(data_path)
+
+        # Preprocess start/end \n in the data
+        for i in range(len(list_data_dict)):
+            for k, v in list_data_dict[i].items():
+                if isinstance(v, str):
+                    list_data_dict[i][k] = v.strip("\n")
+
+        logging.warning("Formatting inputs...")
+
+        sources = [PROMPT.format_map(example) for example in list_data_dict]
+        targets = [
+            f"{example['answer']}{tokenizer.eos_token}" for example in list_data_dict
+        ]
+
+        logging.warning("Tokenizing inputs... This may take some time...")
+        data_dict = preprocess(sources, targets, tokenizer)
+
+        self.input_ids = data_dict["input_ids"]
+        self.labels = data_dict["labels"]
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
+        return dict(input_ids=self.input_ids[i], labels=self.labels[i])
+
+
+@dataclass
+class DataCollatorForSupervisedDataset(object):
+    """Collate examples for supervised fine-tuning."""
+
+    tokenizer: transformers.PreTrainedTokenizer
+
+    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        input_ids, labels = tuple(
+            [instance[key] for instance in instances] for key in ("input_ids", "labels")
+        )
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
+        )
+        labels = torch.nn.utils.rnn.pad_sequence(
+            labels, batch_first=True, padding_value=IGNORE_INDEX
+        )
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
+
+
+def make_supervised_data_module(
+    tokenizer: transformers.PreTrainedTokenizer, data_args
+) -> Dict:
+    """Make dataset and collator for supervised fine-tuning."""
+    train_dataset = SupervisedDataset(
+        tokenizer=tokenizer, data_path=data_args.data_path
+    )
+    data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
+    return dict(
+        train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator
+    )
+
+
+def train():
+
+    # parser
+    parser = transformers.HfArgumentParser(
+        (ModelArguments, DataArguments, TrainingArguments)
+    )
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    # model
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path,
+        load_in_8bit=True,
+        torch_dtype = torch.float16,
+    )
+
+    # tokenizer
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        "meta-llama/Llama-2-7b-chat-hf",
+        model_max_length=training_args.model_max_length,
+        padding_side="right",
+    )
+
+    tokenizer = modify_special_tokens(tokenizer)
+
+
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        inference_mode=False,
+        r=8,
+        lora_alpha=32,
+        lora_dropout=0.05
+        )
+    
+    model = prepare_model_for_kbit_training(model)
+    model = get_peft_model(model, peft_config)
+
+
+
+    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+
+    trainer = Trainer(model=model,
+                         args=training_args,
+                         **data_module,
+                         )
+    trainer.train()
+    trainer.save_state()
+    trainer.save_model(training_args.output_dir)
+
+
+if __name__ == "__main__":
+    train()
