f42cdaf460daa8a95c7eb84355f6c33dd9f422e8
YeonJeong Kim <65206075+yjeong-k@users.noreply.github.com>
2023-12-17 20:36:25 +0900

    Modify PMC-llama.py as the final version

--- code/comparison/PMC-llama.py
+++ code/comparison/PMC-llama.py
@@ -1,10 +1,8 @@
-# NEEDS TO BE MODIFIED!
-
 # Get answers from PMC-Llama baseline model for comparison
 # Input : a CSV file with a column containing answer-generating prompts
-# Output : a CSV file with a newly added column containing each corresponding answer for the prompts
+# Output : a CSV file with a newly added column conatining each corresponding answer for the prompts
 
-import transformers
+from transformers import LlamaTokenizer, LlamaForCausalLM
 import torch
 import argparse
 import pandas as pd
@@ -21,21 +19,27 @@ def main():
     args = parse_args()
     df = pd.read_csv(args.input_path)
 
-    tokenizer = transformers.LlamaTokenizer.from_pretrained('chaoyi-wu/PMC_LLAMA_7B')
-    model = transformers.LlamaForCausalLM.from_pretrained('chaoyi-wu/PMC_LLAMA_7B')
+    tokenizer = LlamaTokenizer.from_pretrained('chaoyi-wu/PMC_LLAMA_7B')
+    model = LlamaForCausalLM.from_pretrained('chaoyi-wu/PMC_LLAMA_7B')
 
     for index, row in df.iterrows():
         prompt = row['prompt']
 
-        batch = tokenizer(
-                    prompt,
-                    return_tensors="pt", 
-                    add_special_tokens=False
-                )
+        input = tokenizer(
+            prompt,
+            return_tensors="pt",
+            add_special_tokens=False
+        )
+        
         with torch.no_grad():
-            generated = model.generate(inputs = batch["input_ids"], max_length=512, do_sample=True, top_k=50)
-            anwer = tokenizer.decode(generated[0])
+            response = model.generate(inputs = input["input_ids"], max_new_tokens=512)
 
+            # find the answer
+            # if a prompt doesn't contain "Response:" at the end, you MUST add it
+            decoded = tokenizer.decode(response[0])
+            answer = decoded.split('Response:')[1].strip()
+            answer = answer.strip('"').strip("'")
+        
         df.at[index, 'PMC-Llama_answer'] = answer
 
     df.to_csv(args.save_path, index=False)
